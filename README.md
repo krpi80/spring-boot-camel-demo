96ffb1bb17ace2dbf0bdcfda89eaf4a7ea0c532e
Krzysztof Pijewski <Krzysztof.Pijewski@gmail.com>
2017-04-01 08:41:57 +0200

    Basic project structure generated from https://start.spring.io.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # spring-boot-camel-demo
-Simple demo of basic features provided by latest spring boot and camel.
+Simple demo of basic features provided by latest Spring Boot and Camel.
+
+# Basics
+
+Project has been generated using (Sprint Initializr)[https://start.spring.io].
---
+++ src/main/java/com/krpi/demo/SpringBootCamelDemoApplication.java
@@ -0,0 +1,12 @@
+package com.krpi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCamelDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCamelDemoApplication.class, args);
+	}
+}
